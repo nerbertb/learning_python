4ab070868a3aa1e3020f59f1e5e718f1447aef52
Nerbs <nerbertb@gmail.com>
2021-04-02 19:32:16 +0800

    router status dictionary within list

--- dictionary.py
+++ dictionary.py
@@ -1,15 +1,27 @@
 
 
 
-items =  {
+item =  [{
 	"hostname" : "router1",
 	"ip-address" : "192.168.1.2",
 	"subnet-mask" : "255.255.255.0",
 	"bgp-status" : "Established",
 	"ipv6-enabled" : False,
 
+},
+
+{
+	"hostname" : "router2",
+	"ip-address" : "192.168.1.3",
+	"subnet-mask" : "255.255.255.0",
+	"bgp-status" : "Established",
+	"ipv6-enabled" : False,
+
 }
 
-items["uptime"] = "00:01:00"
- 
-print (items)
+]
+
+for each in item:
+	for x, y in each.items():
+		print (x,y, "\n")
+	

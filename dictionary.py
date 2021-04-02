d5a81d43e3328104657b7edb772e76bcedba4d84
Nerbs <nerbertb@gmail.com>
2021-04-02 13:53:22 +0800

    dictionary file added

---
+++ dictionary.py
@@ -0,0 +1,15 @@
+
+
+
+items =  {
+	"hostname" : "router1",
+	"ip-address" : "192.168.1.2",
+	"subnet-mask" : "255.255.255.0",
+	"bgp-status" : "Established",
+	"ipv6-enabled" : False,
+
+}
+
+items["uptime"] = "00:01:00"
+ 
+print (items)
